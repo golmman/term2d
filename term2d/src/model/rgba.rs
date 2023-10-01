83d15f810e66c82170a5cf61b4d708673c62e14d
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-10-01 20:48:29 +0200

    add convenient constructors for Rgba

--- term2d/src/model/rgba.rs
+++ term2d/src/model/rgba.rs
@@ -21,6 +21,19 @@ impl Default for Rgba {
 }
 
 impl Rgba {
+    pub const fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self { r, g, b, a }
+    }
+
+    /// Constructs an Rgba value from a hex str.
+    pub const fn from_hex(hex: &str) -> Self {
+        let bytes = hex.as_bytes();
+        let r = 16 * char_to_hex(bytes[0]) + char_to_hex(bytes[1]);
+        let g = 16 * char_to_hex(bytes[2]) + char_to_hex(bytes[3]);
+        let b = 16 * char_to_hex(bytes[4]) + char_to_hex(bytes[5]);
+        Self { r, g, b, a: 255 }
+    }
+
     pub const fn transparent() -> Self {
         Self {
             r: 0,
@@ -154,5 +167,79 @@ impl Rgba {
         }
 
         0
+    }
+}
+
+const fn char_to_hex(ascii_char: u8) -> u8 {
+    match ascii_char {
+        48 => 0,
+        49 => 1,
+        50 => 2,
+        51 => 3,
+        52 => 4,
+        53 => 5,
+        54 => 6,
+        55 => 7,
+        56 => 8,
+        57 => 9,
+        65 | 97 => 10,
+        66 | 98 => 11,
+        67 | 99 => 12,
+        68 | 100 => 13,
+        69 | 101 => 14,
+        70 | 102 => 15,
+        _ => panic!("Not a valid hex string."),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_constructs_rgba_from_hex_string() {
+        let color = Rgba::from_hex("012345");
+        assert_eq!(
+            color,
+            Rgba {
+                r: 1,
+                g: 35,
+                b: 69,
+                a: 255
+            }
+        );
+
+        let color = Rgba::from_hex("678901");
+        assert_eq!(
+            color,
+            Rgba {
+                r: 103,
+                g: 137,
+                b: 01,
+                a: 255
+            }
+        );
+
+        let color = Rgba::from_hex("abcdef");
+        assert_eq!(
+            color,
+            Rgba {
+                r: 171,
+                g: 205,
+                b: 239,
+                a: 255
+            }
+        );
+
+        let color = Rgba::from_hex("ABCDEF");
+        assert_eq!(
+            color,
+            Rgba {
+                r: 171,
+                g: 205,
+                b: 239,
+                a: 255
+            }
+        );
     }
 }
